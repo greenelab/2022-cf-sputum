80905563c9024690035b8799638fb9370974909b
Taylor Reiter <taylorreiter@gmail.com>
2022-03-29 15:34:36 -0700

    modify add to comp snkfl to also process lab samples

--- scripts/snakemake_quant_collect.R
+++ scripts/snakemake_quant_collect.R
@@ -23,23 +23,24 @@ annotation_map <- read_csv(snakemake@input[["annot_map"]], col_names = c("tx_nam
   select(-tx_name2)
 #annotation_map <- read_csv("~/Downloads/pa14_gene_names.csv", col_names = c("tx_name", "tx_name2", "Name")) %>%
 #  select(-tx_name2)
-sf_files <- unlist(snakemake@input[["quant"]])
-#sf_files <- Sys.glob("outputs/salmon/pa14/*/quant.sf")
+sf_files_srx <- unlist(snakemake@input[["quant_srx"]])
+#sf_files_srx <- Sys.glob("outputs/salmon/pa14/*/quant.sf")
+sf_files_spu <- unlist(snakemake@input[["quant_spu"]])
 
 sf <- sf_files %>%
-  set_names() %>%                                                    # add filename as a column
-  map_dfr(read_tsv, show_col_types = FALSE, .id = "srx") %>%                                 # read in files
-  mutate(srx = basename(dirname(srx))) %>%                           # edit filename to srx ID
-  left_join(annotation_map, by = c("Name" = "tx_name")) %>%          # join to gene name
-  select(srx, Name = Name.y, Length, EffectiveLength, TPM, NumReads) # drop transcript name
+  set_names() %>%                                                       # add filename as a column
+  map_dfr(read_tsv, show_col_types = FALSE, .id = "sample") %>%         # read in files
+  mutate(srx = basename(dirname(sample))) %>%                           # edit filename to srx ID
+  left_join(annotation_map, by = c("Name" = "tx_name")) %>%             # join to gene name
+  select(sample, Name = Name.y, Length, EffectiveLength, TPM, NumReads) # drop transcript name
 
 numreads <- sf %>%
-  select(srx, Name, NumReads) %>%                                       # select NumReads
-  pivot_wider(id_cols = Name, values_from = NumReads, names_from = srx) # transform long to wide
+  select(sample, Name, NumReads) %>%                                       # select NumReads
+  pivot_wider(id_cols = Name, values_from = NumReads, names_from = sample) # transform long to wide
   
 tpm <- sf %>%
-  select(srx, Name, TPM) %>%                                       # select TPM
-  pivot_wider(id_cols = Name, values_from = TPM, names_from = srx) # transform long to wide
+  select(sample, Name, TPM) %>%                                       # select TPM
+  pivot_wider(id_cols = Name, values_from = TPM, names_from = sample) # transform long to wide
 
 # write out tables for NumReads and TPM
 write_csv(numreads, snakemake@output[['numreads']])
